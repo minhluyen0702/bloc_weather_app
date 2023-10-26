5bd58f2180dba22180dbfa48bfbb04f9caa1eb29
minhluyen0702 <minhluyen.tran0702@gmail.com>
2023-10-26 21:56:24 +0700

    A first Flutter project using Bloc state management.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,14 +3,17 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
-import 'package:weather/weather.dart';
-import 'package:weather/weather.dart';
 
 import '../bloc/weather_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
